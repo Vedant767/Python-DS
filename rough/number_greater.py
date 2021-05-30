8706600b55b9bb0676937b16b7b124e9bed606da
Vedant <vedantgire48@gmail.com>
2021-05-30 19:28:43 +0530

    Added new array program

--- rough/number_greater.py
+++ rough/number_greater.py
@@ -1,2 +1,14 @@
-n = [11,32,53,4,5,0,8,65,2]
-print(min(n[:2]))
+from functools import reduce
+
+# def add(x):
+#     return x+2
+
+# mylst = [2, 4, 5, 6, 7, 8, 9]
+# filtered = reduce(add, mylst)
+# print(filtered)
+
+def add(x, y ):
+    return x+y
+
+list = [2, 4, 7, 3, 5]
+print(reduce(add,list))
